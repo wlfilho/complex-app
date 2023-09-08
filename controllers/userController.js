fd81e82dd697f0a0db179ff00a01976de8570773
wlfilho <william@jardinsurbanos.com.br>
2023-09-08 16:03:08 -0300

    criado o router e os controllers

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+exports.login = function() {
+
+}
+
+
+exports.logout = function() {
+    
+}
+
+
+exports.register = function(req, res) {
+    res.send("Thanks for registering ")
+    
+}
+
+
+exports.home = function(req, res) {
+    res.render('home-guest')
+}
