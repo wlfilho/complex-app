b99bee7850f072a6d9f66d0dd3f3a6a6f49afcdc
wlfilho <william@jardinsurbanos.com.br>
2023-09-09 00:09:21 -0300

    aula 53

--- models/User.js
+++ models/User.js
@@ -6,6 +6,19 @@ let User = function(data) {
     this.errors = []
 }
 
+User.prototype.cleanUp = function() {
+    if (typeof(this.data.username) != "string") {this.data.username = ""}
+    if (typeof(this.data.email) != "string") {this.data.email = ""}
+    if (typeof(this.data.password) != "string") {this.data.password = ""}
+
+    // get rid of any bogus properties
+    this.data = {
+        username: this.data.username.trim().toLowerCase(),
+        email: this.data.email.trim().toLowerCase(),
+        password: this.data.password,
+    }
+}
+
 User.prototype.validate = function() {
     if (this.data.username == "") {this.errors.push("You must provide a valid username")}
     if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
@@ -19,6 +32,7 @@ User.prototype.validate = function() {
 
 User.prototype.register = function() {
     // Step #1: Validate User data
+    this.cleanUp()
     this.validate()
 
     // Step 2: Only if there are no validation errors
