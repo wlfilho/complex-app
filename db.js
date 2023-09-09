7fded53a5b2d9f439065c72cb06ff3b5d3cb991b
wlfilho <william@jardinsurbanos.com.br>
2023-09-09 01:22:29 -0300

    aula 56 + env file

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const {MongoClient} = require('mongodb')
 
-const client = new MongoClient('mongodb+srv://todoAppUser:oliveira123@cluster0.6a5jlez.mongodb.net/ComplexApp')
+const client = new MongoClient(process.env.CONNECTIONSTRING)
 
 async function start() {
     await client.connect()
     module.exports = client.db()
     const app = require('./app')
-    app.listen(3000)
+    app.listen(process.env.PORT)
 }   
 
 
